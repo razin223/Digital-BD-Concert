bdf28778f9efe80c8ebbc66f7d1bd7ca7b2509ee
razin223 <razin223@gmail.com>
2021-12-03 02:15:10 +0600

    Update ticket_check.blade.php

--- resources/views/new-admin/User/ticket_check.blade.php
+++ resources/views/new-admin/User/ticket_check.blade.php
@@ -11,7 +11,7 @@
                         <div class="col-md-4">
                             <b>Ticket Code</b>
                             <input type="text" name="ticket" value="" class="form-control"/>
-                        </div>
+                        </div><br/>
                         <div class="col-md-4 text-center">
                             <input type="submit" value="Search" class="btn btn-primary"/>
                         </div>
