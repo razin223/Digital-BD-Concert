492cbe739bcda844015be72f242fd906a55d8b57
razin223 <razin223@gmail.com>
2021-12-03 02:26:11 +0600

    Update ticket_check.blade.php

--- resources/views/new-admin/User/ticket_check.blade.php
+++ resources/views/new-admin/User/ticket_check.blade.php
@@ -23,7 +23,7 @@
             </div>
 
             <?php
-            if (isset(request()->input('ticket'))) {
+            if (!empty(request()->input('ticket'))) {
                 $Class = "bg-danger";
                 if ($Data != null) {
                     $Class = "bg-success";
