2888efc9226f2e9346f8be074c50f87d7180d89c
razin223 <razin223@gmail.com>
2021-12-03 02:21:45 +0600

    Update ticket_check.blade.php

--- resources/views/new-admin/User/ticket_check.blade.php
+++ resources/views/new-admin/User/ticket_check.blade.php
@@ -10,7 +10,7 @@
                     <div class="row">
                         <div class="col-md-4">
                             <b>Ticket Code</b>
-                            <input type="text" name="ticket" value="" class="form-control"/>
+                            <input type="number" name="ticket" id="ticket" value="" class="form-control"/>
                         </div>
                     </div>
                     <div class="row">
@@ -23,10 +23,57 @@
             </div>
 
             <?php
+            if (isset($Data)) {
+                $Class = "bg-danger";
+                if ($Data != null) {
+                    $Class = "Success";
+                }
+                ?>
+                <div class="row">
+                    <div class="col-md-4 offset-md-4">
+                        <table class="table table-bordered table-striped table-condensed">
+                            <tbody>
+                                <tr>
+                                    <td colspan="2" class="text-center">
+                                        <h3 class="text-white {{$Class}} ">DBD2021-{{request()->input('ticket')}}</h3>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>Name: </td>
+                                    <td>{{$Data->name}}</td>
+                                </tr>
+                                <tr>
+                                    <td>Email: </td>
+                                    <td>{{$Data->email}}</td>
+                                </tr>
+                                <tr>
+                                    <td>Mobile: </td>
+                                    <td>{{$Data->mobile_no}}</td>
+                                </tr>
+                                <tr>
+                                    <td>Gender: </td>
+                                    <td>{{$Data->gender}}</td>
+                                </tr>
+                                <tr>
+                                    <td>Occupation: </td>
+                                    <td>{{$Data->occupation}}</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+                <?php
+            }
             ?>
         </div>
 
     </div>
 
 </div>
+
+<script type="text/javascript">
+    $(document).ready(function () {
+        $("#ticket").focus();
+    });
+</script>
 @endsection
