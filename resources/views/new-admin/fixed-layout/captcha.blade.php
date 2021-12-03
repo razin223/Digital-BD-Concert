6aac040ebb0ff9cd5a67d3abc246279f1a95dea8
razin223 <razin223@gmail.com>
2021-12-03 22:16:30 +0600

    Update captcha.blade.php

--- resources/views/new-admin/fixed-layout/captcha.blade.php
+++ resources/views/new-admin/fixed-layout/captcha.blade.php
@@ -1,4 +1,4 @@
-<div class="form-group mt-2" style="position: relative; ">
+<div class="form-group mt-5" style="position: relative; ">
     <b>ক‌্যাপচা</b><span class="text-danger">*</span><br/>
     <div id="captcha_zone" style="position:  relative; top: 0; left: 0; width: 100%; border: solid #c9c8c8 1px;">
         <img src="{{captcha_src()}}" id="captcha_img" style="width: 85%; height: 70px;"/>
