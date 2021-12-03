2c5d00a792435ac4b1cd28b70be3e2fb3b6ef661
razin223 <razin223@gmail.com>
2021-12-03 22:19:38 +0600

    Update captcha.blade.php

--- resources/views/new-admin/fixed-layout/captcha.blade.php
+++ resources/views/new-admin/fixed-layout/captcha.blade.php
@@ -4,5 +4,5 @@
         <img src="{{captcha_src()}}" id="captcha_img" style="width: 85%; height: 70px;"/>
         <a href="javascript:;" style="position:  relative; margin-top: 20px;" id="captcha"> <img src="assets/icon/refresh.png" style="width: 12%" class="rounded-circle"/></a>
     </div>
-    <input type="text" name="captcha" class="form-control form-control-lg mt-5" id="exampleInputPassword1" placeholder="উপরের ছবিতে দেখানো অক্ষর গুলো প্রবেশ করান">
+    <input type="text" name="captcha" class="form-control form-control-lg mt-3" id="exampleInputPassword1" placeholder="উপরের ছবিতে দেখানো অক্ষর গুলো প্রবেশ করান">
 </div>
